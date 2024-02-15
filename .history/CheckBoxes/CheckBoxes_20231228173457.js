7de2aa02f01ba16b1fa4ad162aff2181abee7154
TyTyDaCodingGuy <137126638+TyTyDaCodingGuy@users.noreply.github.com>
2024-02-15 01:12:13 +0000

    commit

---
+++ .history/CheckBoxes/CheckBoxes_20231228173457.js
@@ -0,0 +1,8 @@
+document.getElementById("myButton").onclick = function(){
+    if(document.getElementById("muCheckBox").checked){
+        console.log("Success!");
+    }
+    else{
+        console.log("Failed!");
+    };
+};
